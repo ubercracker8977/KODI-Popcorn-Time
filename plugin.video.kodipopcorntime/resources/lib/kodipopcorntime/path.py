bd43c2869fa65ee318a3eca425930be2ecea7990
Henrik Ankersø <privat@diblo.dk>
2015-07-26 05:25:39 +0200

    "path.py" replaces "common.py"

---
+++ plugin.video.kodipopcorntime/resources/lib/kodipopcorntime/path.py
@@ -0,0 +1,14 @@
+﻿#!/usr/bin/python
+import os, xbmc, sys
+
+__addon__ = sys.modules['__main__'].__addon__
+
+def cacheDir():
+    dir = xbmc.translatePath("special://profile/addon_data/%s/cache" % __addon__.getAddonInfo("id"))
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+    return dir
+CACHE_DIR = cacheDir()
+
+RESOURCES_PATH = os.path.join(__addon__.getAddonInfo('path'), 'resources')
+

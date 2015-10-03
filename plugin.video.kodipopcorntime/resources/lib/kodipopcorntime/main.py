55dc46b8a1c05122d861456a84d902653d073538
Henrik Ankersø <privat@diblo.dk>
2015-10-03 10:00:54 +0200

    Fix bad structure 4

--- plugin.video.kodipopcorntime/resources/lib/kodipopcorntime/main.py
+++ plugin.video.kodipopcorntime/resources/lib/kodipopcorntime/main.py
@@ -425,10 +425,11 @@ def run():
         params = dict(urlparse.parse_qsl(settings.addon.cur_uri))
         if not params.pop('cmd', None):
             try:
-                if not settings.addon.version+"~2" == settings.addon.last_update_id:
+                separation = '.2'
+                if not settings.addon.version+separation == settings.addon.last_update_id:
                     # Clear cache after update
                     clear_cache(settings.addon.cache_path)
-                    __addon__.setSetting("last_update_id", settings.addon.version+"~1")
+                    __addon__.setSetting("last_update_id", settings.addon.version+separation)
                 else:
                     # Clean debris from the cache dir
                     for mediaType in ['movies', 'tvshows']:
--- plugin.video.kodipopcorntime/resources/lib/kodipopcorntime/providers/movies/metadata_tmdb.py
+++ plugin.video.kodipopcorntime/resources/lib/kodipopcorntime/providers/movies/metadata_tmdb.py
@@ -1,12 +1,13 @@
 ﻿#!/usr/bin/python
 import time
 from kodipopcorntime.utils import Cache
+from kodipopcorntime.logging import log, LOGLEVEL, log_error
+
+FALLBACKLANG = 'en' # Or None
 
 _api_key = "308a68c313ff66d165c1eb029b0716bc"
 _base_url = "http://api.themoviedb.org"
 _images_base_url = None
-
-FALLBACKLANG = 'en' # Or None
 
 class _Data():
     _limit     = 40
@@ -31,6 +32,65 @@ class _Data():
             _Data._time = time.time()
 
         _Data._count = _Data._count+1
+
+def _credits(credits):
+    castandrole = []
+    director = []
+    writer = []
+
+    for c in credits.get("cast", []):
+        castandrole.append((c["name"], c.get("character", '')))
+
+    for c in credits.get("crew", []):
+        if c["job"] == 'Director':
+            director.append(c["name"])
+            continue
+        if c["job"] == 'Novel' or c["job"] == 'Writer' or c["job"] == 'Screenplay':
+            writer.append(c["name"])
+
+    return {
+        'castandrole': castandrole,
+        'director': u" / ".join(director),
+        'writer': u" / ".join(writer)
+    }
+
+def _info(meta, title=''):
+    overview = meta.get('overview', '')
+    vote_average = meta.get('vote_average')
+    item = {
+        "title": title,
+        "year": int(meta.get("release_date", '0').split("-").pop(0)),
+        "originaltitle": meta.get("original_title", ''),
+        "genre": u" / ".join(g["name"] for g in meta.get("genres", [])),
+        "plot": overview,
+        "plotoutline": overview,
+        "tagline": meta.get("tagline", ''),
+        "rating": float(vote_average or 0.0),
+        "duration": int(meta.get("runtime") or 0),
+        "code": meta.get("imdb_id"),
+        "studio": u" / ".join([s['name'] for s in meta.get("production_companies", [])]),
+        "votes": vote_average and float(meta.get("vote_count")) or 0.0
+    }
+
+    credits = meta.get("credits")
+    if credits:
+        item.update(_credits(credits))
+
+    return item
+
+def _stream_info(meta):
+    return {
+        "video": {
+            "duration": int((meta.get("runtime") or 0)*60)
+        }
+    }
+
+def _properties(meta):
+    fanart = meta.get("fanart", '')
+    if _Data.imageUrl and fanart:
+        fanart = "%s/w500%s" %(_Data.imageUrl, fanart)
+
+    return {"fanart_image": fanart}
 
 def pre():
     cache = Cache("movies.metadata.imdb.conf", ttl=24 * 3600, readOnly=True)
@@ -67,70 +127,23 @@ def item(id, label, year, lang):
     }
 
 def build_item(meta, id, label, year, lang):
-    if meta.get('status_code'):
+    log(str(meta))
+    if not meta or meta.get('status_code'):
         return {}
 
     title = meta.get("title", '')
+
+    poster = meta.get("poster_path", '')
+    if _Data.imageUrl and poster:
+        poster = "%s/w500%s" %(_Data.imageUrl, poster)
+
     item = {
         "label": title,
-        "info": {
-            "title": title,
-            "year": int(meta.get("release_date") and meta["release_date"].split("-")[0] or 0),
-            "originaltitle": meta.get("original_title", ''),
-            "genre": u" / ".join(g["name"] for g in meta.get("genres", [])),
-            "plot": meta.get("overview", ''),
-            "plotoutline": meta.get("overview", ''),
-            "tagline": meta.get("tagline", ''),
-            "rating": float(meta.get("vote_average") or 0.0),
-            "duration": int(meta.get("runtime") or 0),
-            "code": meta.get("imdb_id"),
-            "studio": u" / ".join([s['name'] for s in meta.get("production_companies", [])]),
-            "votes": meta.get("vote_average") and float(meta.get("vote_count")) or 0.0
-        },
-        "stream_info": {
-            "video": {
-                "duration": int((meta.get("runtime") or 0)*60)
-            }
-        }
+        "icon": poster,
+        "thumbnail": poster
     }
-
-    if _Data.imageUrl:
-        poster = meta.get("poster_path")
-        if poster:
-            poster = "%s/w500%s" %(_Data.imageUrl, poster)
-
-        fanart = meta.get("backdrop_path")
-        if fanart:
-            fanart = "%s/original%s" %(_Data.imageUrl, fanart)
-
-        item.update({
-            "icon": poster,
-            "thumbnail": poster,
-            "properties": {
-                "fanart_image": fanart
-            }
-        })
-
-    credits = meta.get("credits")
-    if credits:
-        castandrole = []
-        director = []
-        writer = []
- 
-        for c in credits.get("cast", []):
-            castandrole.append((c["name"], c.get("character", '')))
-
-        for c in credits.get("crew", []):
-            if c["job"] == 'Director':
-                director.append(c["name"])
-                continue
-            if c["job"] == 'Novel' or c["job"] == 'Writer' or c["job"] == 'Screenplay':
-                writer.append(c["name"])
-
-        item.setdefault('info', {}).update({
-            'castandrole': castandrole,
-            'director': u" / ".join(director),
-            'writer': u" / ".join(writer)
-        })
+    item.setdefault('info', {}).update(_info(meta, title))
+    item.setdefault('stream_info', {}).update(_stream_info(meta))
+    item.setdefault('properties', {}).update(_properties(meta))
 
     return item
